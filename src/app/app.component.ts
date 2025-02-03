f4d703d1399833f0ae7cae72eee0178ee748796e
Vishal Zade <vishalzade75@gmail.com>
2025-02-03 17:42:46 +0530

    Observable use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Observable';
+
+  ngOnInit(): void {
+    const obs =  new  Observable (observer => {
+      observer.next('1'),
+        observer.next('2'),
+        observer.next('3'),
+
+        observer.complete()
+    })
+
+
+    obs.subscribe({
+      next: x => console.log(x),
+      error: err => console.error('something wrong occurred: ' + err),
+      complete: () => console.log('done'),
+    });
+  }
+
+
 }
